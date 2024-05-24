50d942867600bb0675ea84e6b23b8d0504bfe6c8
Prince34998 <112163728+Prince34998@users.noreply.github.com>
2024-05-24 18:06:33 +0530

    part 4 done

--- App.py
+++ App.py
@@ -1,4 +1,6 @@
 import sys
+import datetime
+import os
 
 # Custom exceptions for error handling
 class InvalidNameError(Exception):
@@ -38,6 +40,10 @@ class Customer:
     def get_reward(self):
         pass
     
+    def get_current_reward(self):
+        """Returns the customer's reward points."""
+        return self.reward
+
     def get_discount(self, total_cost):
         pass
     
@@ -47,7 +53,7 @@ class Customer:
     def display_info(self):
         pass
 
-# Basic Customer class subtype of customer
+# Basic Customer class, a subtype of customer
 class BasicCustomer(Customer):
     reward_rate = 1.0  # Default flat reward rate (100%)
 
@@ -59,6 +65,9 @@ class BasicCustomer(Customer):
         """Calculates and returns the reward points based on total cost."""
         return round(total_cost * self.reward_rate)
     
+    def get_current_reward(self):
+        return super().get_current_reward()
+
     def update_reward(self, value):
         """Updates the reward points for the customer."""
         self.reward += value
@@ -72,7 +81,7 @@ class BasicCustomer(Customer):
         """Sets a new reward rate for all basic customers."""
         cls.reward_rate = new_rate
 
-# VIP Customer class subtype of customer
+# VIP Customer class, a subtype of customer
 class VIPCustomer(Customer):
     reward_rate=1.0
 
@@ -89,6 +98,13 @@ class VIPCustomer(Customer):
         """Calculates and returns the reward points based on total cost."""
         return round(total_cost * self.reward_rate)
     
+    def get_discount_rate(self):
+        """Returns the discount rate of VIP customer"""
+        return self.discount_rate
+    
+    def get_current_reward(self):
+        return super().get_current_reward()
+
     def update_reward(self, value):
         """Updates the reward points for the VIP customer."""
         self.reward += value
@@ -131,16 +147,19 @@ class Product:
         """Returns whether the product requires a prescription."""
         return self.prescription
     
-    def update_price(self, price):
-        self.price = price
+    def update_price(self, new_price):
+        """Updates the product's price with new price"""
+        self.price = new_price
     
     def update_prescription(self, prescription):
+        """Updates the doctor's prescription requirements"""
         self.prescription = prescription
 
     def display_info(self):
         """Displays the product's information."""
         print(f"{self.ID}\t {self.name}\t {self.price:.2f}\t {'YES' if self.prescription == 'y' else 'NO'}\t --".expandtabs(14))
 
+# Bundle class, a subtype of Product
 class Bundle(Product):
     def __init__(self, ID, name, products):
         """Initializes a bundle with an ID, name, and a list of component products and call super class."""
@@ -160,7 +179,7 @@ class Bundle(Product):
 # Order Class
 class Order:
     def __init__(self, customer, products, quantities):
-        """Initializes a new order with a customer, product, and quantity."""
+        """Initializes a new order with a customer, products, and quantities."""
         self.customer = customer
         self.products = products
         self.quantities = quantities
@@ -191,12 +210,47 @@ class Order:
                 final_cost = 0
         return final_cost
 
+# OrderHistory class, a subtype of Order
+class OrderHistory(Order):
+    def __init__(self, customer, products, quantities, total_cost, earned_rewards, date_time):
+        """Initializes a new order history with a customer, products, quantities, total cost, earned rewards and date."""
+        super().__init__(customer, products, quantities)
+        self.total_cost = total_cost
+        self.earned_rewards = earned_rewards
+        self.date_time = datetime.datetime.strftime(date_time, "%d/%m/%Y %H:%M:%S") if isinstance(date_time, datetime.datetime) else date_time
+
+    def display_info(self):
+        """Displays the order history information."""
+        products_str = ', '.join(f'{quantity} x {product.get_id()}' for product, quantity in zip(self.products,self.quantities))
+
+        dt = datetime.datetime.strptime(self.date_time, "%d/%m/%Y %H:%M:%S")
+        readable_string = dt.strftime("%a, %d/%b/%y at %H:%M")
+
+        print(f"{self.customer.get_name():<7} {products_str:<23} {self.total_cost}\t {self.earned_rewards}\t {readable_string}".expandtabs(8))
+
+    def get_customer_id(self):
+        """Returns the customer's id"""
+        return self.customer.get_id()
+    
+    def get_total_cost(self):
+        """Returns the total cost of order"""
+        return self.total_cost
+    
+    def get_earned_rewards(self):
+        """Returns the earned reward of order"""
+        return self.earned_rewards
+    
+    def get_date_time(self):
+        """Returns the date time of order"""
+        return self.date_time
+    
 # Records class
 class Records:
     def __init__(self):
-        """Initializes the Records class with empty customer and product lists."""
+        """Initializes the Records class with empty customer, product lists and order history."""
         self.customers = []
         self.products = []
+        self.order_history =[]
     
     def read_customers(self, filename):
         """Reads customer data from a file and stores them in the customer list."""
@@ -211,6 +265,7 @@ class Records:
                     else:
                         customer_id, name, reward_rate, reward = data
                         customer = BasicCustomer(customer_id, name, int(reward))
+                        BasicCustomer.reward_rate = float(reward_rate)
                         self.customers.append(customer)
         except FileNotFoundError:
             print("Error: Customer file not found!")
@@ -237,6 +292,34 @@ class Records:
             print("Error: Product file not found!")
             sys.exit()
     
+    def read_orders(self, filename):
+        """Reads order history data from a file and stores them in the order history list."""
+        try:
+            with open(filename, 'r') as file:
+                for line in file:
+                    data = line.strip().split(', ')
+                    customer_id_or_name = data[0]
+                    customer = self.find_customer(customer_id_or_name)
+                    total_cost = float(data[-3])
+                    earned_rewards = int(data[-2])
+                    date_time = data[-1]
+
+                    products = []
+                    quantities = []
+                    for i in range(1, len(data) - 3, 2):
+                        product_id_or_name = data[i]
+                        quantity = int(data[i+1])
+                        product = self.find_product(product_id_or_name)
+                        products.append(product)
+                        quantities.append(quantity)
+
+                    order_history = OrderHistory(customer,products,quantities, total_cost, earned_rewards, date_time)
+                    self.order_history.append(order_history)
+                    customer.update_reward(earned_rewards)
+
+        except FileNotFoundError:
+            print("Cannot load the order file.\n")
+
     def find_customer(self, search_value):
         """Finds and returns a customer by their ID or name."""
         for customer in self.customers:
@@ -251,6 +334,10 @@ class Records:
                 return product
         return None
     
+    def find_orders(self, customer):
+        """Find and return the order history of a given customer"""
+        return [history for history in self.order_history if customer.get_id() == history.get_customer_id() ]
+
     def list_customers(self):
         """Lists all existing customers."""
         print("\nExisting Customers:")
@@ -266,6 +353,13 @@ class Records:
             product.display_info()
             # print(product.requires_prescription())
     
+    def list_orders(self):
+        """Lists all completed order's history"""
+        print("\nOrder history of all customers:")
+        print(f"Name\t {'Products':<19} {'Total Cost':<10} Rewards\t Order Time".expandtabs(7))
+        for order in self.order_history:
+            order.display_info()
+
     def highest_id_number(self):
         """Returns the highest customer ID number."""
         last_customer = self.customers[-1]
@@ -283,14 +377,38 @@ class Records:
             new_product = Product(new_id, name, price, prescription)
             self.products.append(new_product) 
 
+    def save_customers(self, filename):
+        """Write the details of current existing customers in the file"""
+        with open(filename, 'w') as file:
+            for customer in self.customers:
+                file.write(f"{customer.get_id()}, {customer.get_name()}, {customer.reward_rate}, {f'{customer.get_discount_rate()}, ' if isinstance(customer, VIPCustomer) else ''}{customer.get_current_reward()}\n")
+
+    def save_products(self, filename):
+        """Write the details of current existing products in the file"""
+        with open(filename, 'w') as file:
+            for product in self.products:
+                if product.get_id().startswith('B'):
+                    file.write(f"{product.get_id()}, {product.get_name()}, {', '.join(product.get_id() for product in product.products)}\n")
+                    
+                else:
+                    file.write(f"{product.get_id()}, {product.get_name()}, {product.get_price()}, {product.requires_prescription()}\n")
+
+    def save_orders(self, filename):
+        """Write the details of completed orders in the file"""
+        with open(filename, 'w') as file:
+            for order in self.order_history:
+                file.write(f"{order.customer.get_name()}, {', '.join(', '.join((product.get_id(), str(quantity))) for product,quantity in zip(order.products, order.quantities))}, {order.get_total_cost()}, {order.get_earned_rewards()}, {order.get_date_time()}\n")
+
 # Operations class
 class Operations:
-    def __init__(self):
-        """Initializes the Operations class and reads customer and product data from files."""
+    def __init__(self, customer_file, product_file, order_file):
+        """Initializes the Operations class and reads customer, product and orders data from files."""
         self.records = Records()
-        self.records.read_customers("CREDIT_DI/customers.txt")
-        self.records.read_products("CREDIT_DI/products.txt")
+        self.records.read_customers(customer_file)
+        self.records.read_products(product_file)
+        self.records.read_orders(order_file)
 
+    # Validation methods
     def validate_customer(self, customer):
         """Validates that the customer name contains only alphabetic characters or ID exists."""
         customer_details = self.records.find_customer(customer)
@@ -342,9 +460,9 @@ class Operations:
             raise InvalidRateError("The reward rate must be a valid positive number greater than 0.")
         return value
 
+    # Menu methods
     def make_purchase(self):
         """Guides the user through the purchase process and prints a receipt."""
-
         while True:
             try:
                 customer_name = input("Enter the name of the customer or ID:\n")
@@ -435,6 +553,10 @@ class Operations:
             # Update customer reward points
             customer.update_reward(reward_points)
 
+            # Store order history
+            order_history = OrderHistory(customer, products, quantities, final_cost, reward_points, datetime.datetime.now())
+            self.records.order_history.append(order_history)
+
     def display_customers(self):
         """Prints a formatted list of all customers with their details."""
         self.records.list_customers()
@@ -493,6 +615,40 @@ class Operations:
                 print(e)
         vip_customer.set_discount_rate(new_rate)
 
+    def display_all_orders(self):
+        """Prints a formatted list of all orders with their details."""
+        self.records.list_orders()
+
+    def display_customer_order_history(self):
+        """Prints a formatted order list of a particular customer with their details."""
+        while True:
+                customer_identifier = input("Enter the name or ID of the customer:\n")
+                customer = self.records.find_customer(customer_identifier)
+                if not customer:
+                    print("Invalid customer. Please enter a valid customer name or ID.")
+                    continue
+                break
+       
+    # Retrieve order history   
+        order_history:OrderHistory = self.records.find_orders(customer)
+    
+    # Display order history
+        print(f"\nThis is the order history of {customer.get_name()}.")
+        print(f"\t{'Products':<30}{'Total Cost':<15}{'Earned Rewards':<15}".expandtabs(10))
+
+        for i, order in enumerate(order_history, 1):
+            products_info = ", ".join(f"{quantity} x {product.get_name()}" for product, quantity in zip(order.products, order.quantities))
+            print(f"Order {i:<4}{products_info:<30}{order.total_cost:<15.2f}{order.earned_rewards:<15}") 
+
+    def save_data(self):
+        """Saves the data to the files"""
+        customer_file, product_file, order_file = command_line_args()
+        self.records.save_customers(customer_file)
+        self.records.save_products(product_file)
+        if os.path.isfile(order_file):
+            self.records.save_orders(order_file)
+        sys.exit()
+
     def display_menu(self):
         """Displays the program menu with available options."""
         print("\n"+"#" * 60)
@@ -503,6 +659,8 @@ class Operations:
         print("4: Add/update information of a product")
         print("5: Adjust the reward rate of all Basic customers")
         print("6: Adjust the discount rate of a VIP customer")
+        print("7: Display all orders")
+        print("8: Display a customer order history")
         print("0: Exit the program")   
         print("#" * 60)
 
@@ -528,14 +686,48 @@ class Operations:
                 self.adjust_basic_customer_reward_rate()
             elif choice == '6':
                 self.adjust_vip_customer_discount_rate()
+            elif choice == '7':
+                self.display_all_orders()
+            elif choice == '8':
+                self.display_customer_order_history()
             elif choice == '0':
                 print("Exiting program...") # Exit message
-                break
+                self.save_data() # Save data before terminate
             else:
                 # Display error if enter incorrect input
                 print("Invalid Choice. Please choose correct option.")
-            
+
+def command_line_args():
+    """Reads file from command line arguments"""
+    # default files
+    customer_file = "customers.txt"
+    product_file = "products.txt"
+    order_file = "orders.txt"
+
+    # If no argument passed through command line it checks for default files
+    if len(sys.argv) == 1:
+        if not os.path.isfile(customer_file):
+            print("Error: Customer file not found in local directory.")
+            sys.exit()
+
+        if not os.path.isfile(product_file):
+            print("Error: Product file not found in local directory.")
+            sys.exit()
+
+    # Show usage if invalid number of arguments passed
+    elif len(sys.argv) < 3 or len(sys.argv) > 4:
+        print("Usage: python pharmacy.py <customer_file> <product_file> [order_file]")
+        sys.exit()
+   
+    else:
+        customer_file = sys.argv[1]
+        product_file = sys.argv[2]
+        order_file = order_file if len(sys.argv) == 3 else sys.argv[3]
+
+    return customer_file, product_file, order_file
+
 # Main program
 if __name__ == "__main__":
-    operations = Operations()
+    customer_file, product_file, order_file = command_line_args()
+    operations = Operations(customer_file, product_file, order_file)
     operations.run()
